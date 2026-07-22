bbfd207fdcb562a3d1e2dc8bd1f894818a5aeea8
kyevegz <jimenacarrillocastillo@gmail.com>
2026-07-22 01:04:28 -0600

    feat: encriptar contrasena de registro con bcrypt

--- routes/registro.routes.js
+++ routes/registro.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
+const bcrypt = require('bcrypt');
 
 //ruta para mostrar el form de registro
 router.get('/registro', (req, res) =>{
@@ -13,19 +14,28 @@ router.post('/registro', async (req, res) => {
         //1 - extra los datos que bienen del for, un req.body
         const {nombre, apellidop, apellidom, fechaNacimiento, usuario, correo, contrasena } = req.body;
 
-        //señal de que sí los recibió, meramente control
-        console.log("DATOS RECIBIDOS: ", req.body);
+        /* 2 - definición del número de rondas de salt, 10 es el estándar seguro y equilibrado
+        un salt es una cadena de caracteres que se añade a la contraseña antes de procesarlam garantiza que el hash (versión cifrada) sea única*/
+        const saltRounds = 10;
 
-        //2 - inserción en la bd, los ? evitan inyección SQL
+        //3 - encriptación de la contraseña
+        const hashedContrasena = await bcrypt.hash(contrasena, saltRounds);
+        
+        
+        
+        //señal de que sí los recibió, meramente control
+        //console.log("DATOS RECIBIDOS: ", req.body);
+
+        //4 - inserción en la bd, los ? evitan inyección SQL
         const query = `INSERT INTO 
             usuarios (usuario, nombre, apellidop, apellidom, fechaNacimiento,correo, contrasena)
             VALUES (?, ?, ?, ?, ?, ?, ?)
         `;
 
-        await db.query(query, [usuario, nombre, apellidop, apellidom, fechaNacimiento,correo, contrasena]);
+        await db.query(query, [usuario, nombre, apellidop, apellidom, fechaNacimiento,correo, hashedContrasena]);
 
         //3 - respuesta temporal para checar si funcionó
-        res.send("USUARIO REGISTRADO");
+        res.send("USUARIO REGISTRADO con bcrypt");
 
     }catch (error){
         console.error("ERROR AL REGISTRAR USUARIO: ", error);
