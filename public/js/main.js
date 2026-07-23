4ce2baa553faa62128ed8cdd93d8d277dfa1f303
kyevegz <jimenacarrillocastillo@gmail.com>
2026-07-22 21:03:10 -0600

    feat: formulario de registro por fases

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
-/*------------------------------------------
-        MENÚ HAMBURGUESA -BOTÓN ABRIR
--------------------------------------------*/
+
 
 document.addEventListener("DOMContentLoaded", () => {
+    /*------------------------------------------
+            MENÚ HAMBURGUESA -BOTÓN ABRIR
+    -------------------------------------------*/
+
+
     const hamburgerBtn = document.querySelector(".nav-mini__hamburguer-button");//selecciona el botón del menú
     const sidebarMenu = document.querySelector(".sidebar--menu");//selecciona todo el sidebar
     // const iconBars = hamburgerBtn.querySelector(".nav-mini__icon--bars");//selecciona la etiqueta i del ícono de barras
@@ -39,7 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     /*------------------------------------------
         BOTÓN VER CONTRASEÑA CON FUNCIÓN
--------------------------------------------*/
+    -------------------------------------------*/
 
     //función para configuración del interruptor
     function configurarInterruptor(botonId, inputId) {
@@ -87,7 +90,82 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-    //es igual a la función anterior pero con el campo de confirmar
+    /*--------------------------------------------------
+    FORMULARIO POR FASES CON VALIDACIÓN Y LOCALSTORAGE
+    ---------------------------------------------------- */
+
+    const form = document.getElementById('registroForm');
+    const paso1 = document.getElementById('paso1');
+    const paso2 = document.getElementById('paso2');
+    const barraProgreso = document.getElementById('barraProgreso');
+    const btnSig = document.getElementById('btnSig');
+    const btnAnter = document.getElementById('btnAnter');
+    const labelPaso1 = document.getElementById('labelPaso1');
+    const labelPaso2 = document.getElementById('labelPaso2');
+
+    if(form && paso1 && paso2){
+        const inputs = form.querySelectorAll('input');
+        
+        //al cargar la página, busca los datos que se hayan guardado en el localstorage
+        inputs.forEach(input =>{
+            const valorGuardado = localStorage.getItem(input.name);
+            const esPassword = input.name.includes('contrasena');
+
+            if(valorGuardado && !esPassword){
+                input.value = valorGuardado;
+            }
+
+            //guardar los datos en tiempo real mientras el usuario está escirbiendi
+            input.addEventListener("input", () =>{
+                if(!esPassword){
+                    localStorage.setItem(input.name, input.value);
+                }
+            });
+        });
+
+        //botón de siguiente, valida el paso 1 antes de seguir
+        btnSig.addEventListener("click", () =>{
+            const inputsPaso1 = paso1.querySelectorAll("input");
+            let todosValidos = true;
+
+            inputsPaso1.forEach(input =>{
+                if(!input.checkValidity()){
+                    todosValidos = false;
+                    input.reportValidity();
+                }
+            });
+
+            if(todosValidos){
+                paso1.classList.replace('form__step--active', 'form__step--hidden');
+                paso2.classList.replace('form__step--hidden', 'form__step--active');
+                
+
+                //actualizar la barra de progreso a 100%
+
+                barraProgreso.style.width = "100%";
+                labelPaso1.classList.remove('form-progress__label--active');
+                labelPaso2.classList.add('form-progress__label--active');
+            }
+        });
+
+        //botón de anterior
+
+        btnAnter.addEventListener("click", () => {
+            paso2.classList.replace('form__step--active', 'form__step--hidden');
+            paso1.classList.replace('form__step--hidden', 'form__step--active');
+
+            barraProgreso.style.width = "50%";
+            labelPaso2.classList.remove('form-progress__label--active');
+            labelPaso1.classList.add('form-progress__label--active');
+        })
+
+        //limpiar el localstorage al enviar con éxito el formulario
+        form.addEventListener("submit", () => {
+            inputs.forEach(input => {
+                localStorage.removeItem(input.name);
+            });
+        });
+    }
 });
 
 
