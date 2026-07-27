0f4e461d7e8d69d693f2180c6bcd0f9eecc3f88a
kyevegz <jimenacarrillocastillo@gmail.com>
2026-07-27 17:00:57 -0600

    fix: solucionar que no imprimia la categoria ya que necesitaba un join en la consulta

--- routes/asesores.route.js
+++ routes/asesores.route.js
@@ -7,7 +7,13 @@ router.get('/asesores', async (req, res) =>{
     try{
         //consultar todos los registros de cateforías
         const [categorias ] =await db.query('SELECT * FROM categorias');
-
+        const [asesores] = await db.query(
+            `SELECT nombre, apellidop, apellidom, gradoEscolar, 
+            descripcion, inicioTurno, finTurno, cat.nombreCat 
+            FROM asesores
+            JOIN categorias cat ON asesores.idCategoria = cat.id
+            `
+        );
         /*se lee la categorías que el usuario seleccionó en la url, 
         por ejemplo, ?categoria=emprendimiento
         Si no encuentra, se sobreentiende que quiere ver todos los asesores
@@ -17,10 +23,12 @@ router.get('/asesores', async (req, res) =>{
         // se escribe un filtro ?filtro=valor en la url
         const categoriaSeleccionada = req.query.categoria || 'todas';
 
+        console.log(categorias)
         //se envían los datos de la vista para que en ejs sean dibujados y plasmados en el navtabs
         res.render('asesores/index', {
-            categorias: categorias,
-            categoriaSeleccionada: categoriaSeleccionada
+            categorias,
+            categoriaSeleccionada, 
+            asesores
         });
     }catch(error){
         console.error("Error al cargar la página de asesores.", error);
