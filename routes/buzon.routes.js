6ac32c06b3c8614eebbe1442b0c8a25ca88b1d44
kyevegz <jimenacarrillocastillo@gmail.com>
2026-07-27 01:45:45 -0600

    feat: anadir estructura y estilos de la pagina de faq

--- routes/buzon.routes.js
+++ routes/buzon.routes.js
@@ -4,11 +4,12 @@ const db = require('../config/db');
 const jwt = require('jsonwebtoken');
 
 const rutasValidas = ['enviar', 'faq'];
+
 router.get('/buzon/:option', (req, res) => {
     const {option} = req.params;
 
     if(!rutasValidas.includes(option)){
-        return res.status(400).render('404');
+        return res.status(404).render('404');
     }
 
     res.render(`buzon/${option}`);
