46c03bf9fc9908f469d778f8a734bbeb16650f8c
kyevegz <jimenacarrillocastillo@gmail.com>
2026-07-27 21:43:06 -0600

    feat: anadir paginacion en asesores

--- routes/asesores.route.js
+++ routes/asesores.route.js
@@ -8,6 +8,13 @@ router.get('/asesores', async (req, res) =>{
         //consultar todos los registros de cateforías
         const [categorias ] =await db.query('SELECT * FROM categorias');
         let asesores = [];
+        const limit = 9;
+        let paginaActual = parseInt(req.query.page) || 1;
+        let totalPaginas = 0, offset = (paginaActual - 1) * limit;
+        
+
+        //si no trae el parámetro page, es entonces la número 1
+        
 
 
         /*se lee la categorías que el usuario seleccionó en la url, 
@@ -20,30 +27,53 @@ router.get('/asesores', async (req, res) =>{
         let categoriaSeleccionada = req.query.categoria || 'todas';
         const existeCategoria = categorias.some(cat => cat.nombreCat === categoriaSeleccionada);
 
-        console.log(categoriaSeleccionada)
+        //console.log(categoriaSeleccionada)
 
         if(!existeCategoria){
             categoriaSeleccionada = 'todas';
-            [asesores] = await db.query(
-                `SELECT nombre, apellidop, apellidom, gradoEscolar, 
-                descripcion, inicioTurno, finTurno, cat.nombreCat 
-                FROM asesores
-                JOIN categorias cat ON asesores.idCategoria = cat.id`
-            );
-        }else{
+            
+            const [[totalRegistros]] = await db.query(`
+                SELECT COUNT(*) AS total FROM asesores
+            `);
+            totalPaginas = Math.ceil(totalRegistros.total/limit);
+
+            
             [asesores] = await db.query(
                 `SELECT nombre, apellidop, apellidom, gradoEscolar, 
                 descripcion, inicioTurno, finTurno, cat.nombreCat 
                 FROM asesores
                 JOIN categorias cat ON asesores.idCategoria = cat.id
-                WHERE cat.nombreCat = ?`
-            ,[categoriaSeleccionada]);
+                LIMIT ? OFFSET ?
+                `, [limit, offset]
+            );
+            
+        }else{
+            const [[totalRegistros]] = await db.query(`
+                SELECT COUNT(*) AS total
+                FROM asesores 
+                JOIN categorias cat ON asesores.idCategoria = cat.id
+                WHERE cat.nombreCat =?`, 
+                [categoriaSeleccionada]);
+            totalPaginas = Math.ceil(totalRegistros.total/limit);
+
+            [asesores] = await db.query(
+                `SELECT nombre, apellidop, apellidom, gradoEscolar, 
+                descripcion, inicioTurno, finTurno, cat.nombreCat
+                FROM asesores
+                JOIN categorias cat ON asesores.idCategoria = cat.id
+                WHERE cat.nombreCat = ?
+                LIMIT ? OFFSET ?`
+            ,[categoriaSeleccionada, limit, offset]);
         }
+
+        
 
         console.log(categorias)
         //se envían los datos de la vista para que en ejs sean dibujados y plasmados en el navtabs
         res.render('asesores/index', {
+            totalPaginas,
             categorias,
+            paginaActual,
             categoriaSeleccionada, 
             asesores
         });
