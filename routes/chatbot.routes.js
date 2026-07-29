562a811317d24771fbf810135e9b8d41c983578d
kyevegz <jimenacarrillocastillo@gmail.com>
2026-07-29 12:32:33 -0600

    fix: solucionar codigo obsoleto de version anterior de api de gemini

--- routes/chatbot.routes.js
+++ routes/chatbot.routes.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const {GoogleGenerativeAI} = require('@google/genai');
+const {GoogleGenAI} = require('@google/genai');
 
 //se importa para proteger la ruta de los no logueados
 const verificarToken = require('../middlewares/auth.js');
 
 //inicializar el sdk de gemini con la variable de entorno secreta
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const ai = new GoogleGenAI({apiKey: process.env.GEMINI_API_KEY});
 
 //elegir el modelo, se usa flash porque el es más rápido y eficiente para chatbots
-const modelo = genAI.getGenerativeModel({
-    model: 'gemini-1.5-flash',
-    systemInstruccion: `Eres MarIA, un asistente virtual amable y 
-    experto en los Objetivos de Desarrollo Sostenible (ODS) y la 
-    Agenda 2030. Trabajas para la plataforma Nova 2030. Tus 
-    respuestas deben ser precisas, amigables y enfocadas en 
-    guiar a los estudiantes a crear proyectos de impacto social. 
-    Evita respuestas extremadamente largas.`
-});
+// const modelo = genAI.getGenerativeModel({
+//     model: 'gemini-1.5-flash',
+//     systemInstruccion: `Eres MarIA, un asistente virtual amable y 
+//     experto en los Objetivos de Desarrollo Sostenible (ODS) y la 
+//     Agenda 2030. Trabajas para la plataforma Nova 2030. Tus 
+//     respuestas deben ser precisas, amigables y enfocadas en 
+//     guiar a los estudiantes a crear proyectos de impacto social. 
+//     Evita respuestas extremadamente largas.`
+// });
 
 //ruta post para recibir los mensajes, se protege con la función de verificar token
 router.post('/api/chat', verificarToken, async (req, res) => {
@@ -31,14 +31,25 @@ router.post('/api/chat', verificarToken, async (req, res) => {
             });
         }
 
-        //llamada asíncrona a la IA de google
-        const resultado = await modelo.generateContent(mensaje);
-        const respuestaIA = await resultado.response.text();
+        //llamar al modelo, pasando la configuración integrada
+        const responde = await ai.models.generateContent({
+            model: 'gemini-1.5-flash',
+            contents: mensaje,
+            config: {
+                systemInstruccion: `Eres MarIA, un asistente virtual amable y 
+                experto en los Objetivos de Desarrollo Sostenible (ODS) y la 
+                Agenda 2030. Trabajas para la plataforma Nova 2030. Tus 
+                respuestas deben ser precisas, amigables y enfocadas en 
+                guiar a los estudiantes a crear proyectos de impacto social. 
+                Evita respuestas extremadamente largas.`
+            }
+        });
+
 
         //enviar la repsuesta formateada al cliente
         return res.status(200).json({
             emisor: 'MarIA',
-            mensaje: respuestaIA
+            mensaje: response.text
         });
 
     }catch(error){
