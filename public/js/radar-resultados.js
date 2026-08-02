ad4959e63ecbac3421494bcfd09a62c6c8c7d5da
kyevegz <jimenacarrillocastillo@gmail.com>
2026-08-01 21:47:49 -0600

    feat: anadir codigo js para usar chart js

---
+++ public/js/radar-resultados.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const contexto = document.getElementById('radarChart');
+    if(!contexto) return;
+
+    /*el puntajes radar, lo que se recibe desde 
+    node, se inyecta con ejs en el html*/
+
+    const labels = Object.keys(puntajesRadar);
+    const dataValues = Object.values(puntajesRadar);
+
+    new Chart( contexto, {
+        type: 'radar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Tus puntajes destacados',
+                data: dataValues,
+
+                //estilos
+                backgroundColor: 'rgba(16, 168, 154, 0.2)',
+                borderColor: '#10bc9a',
+                pointBackgroundColor: '#099078',
+                pointBorderColor:'#fff',
+                pointHoverBackgroundColor:'#fff',
+                pointHoverBorderColor: '#099078',
+                borderWidth: 2,
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                r: {
+                    angleLines: {color: 'rgba(0,0,0,0.1)'},
+                    grid: {color: 'rgab(0,0,0,0.1)'},
+                    pointLabels: {
+                        font: {
+                            family: '"Nunito", sans-serif',
+                            size: 12,
+                            weight: 'bold'
+                        },
+                        color: '#4a5363'
+                    },
+                    ticks: {
+                        display: false
+                    }
+                }
+            },
+            plugins: {
+                legend: {display: false}, //oculta la leyende superior
+                tooltip: {
+                    backgroundColor: '#15181e',
+                    titleFont: {family: '"Nunito", sans-serif', size: 14},
+                    bodyFont: {family: '"Nunito", sans-serif', size: 14},
+                    padding: 12,
+                    cornerRadius: 8
+                }
+            }
+        }
+    });
+});
